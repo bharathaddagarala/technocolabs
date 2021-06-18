0a0c5529ce847727d3f58b63f8b6089a56863dfd
bharathaddagarala <85801717+bharathaddagarala@users.noreply.github.com>
2021-06-18 15:46:17 +0530

    Create index.js

---
+++ projecttask6/index.js
@@ -0,0 +1,10 @@
+const kelvin = 293;
+//constant value kelvin =293
+const celsius = kelvin-273;
+//constant value celsius = kelvin-273
+let fahrenheit =  celsius * (9/5) + 32;
+//  given equation
+fahrenheit = Math.floor(fahrenheit);
+//math.floor method
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
+//console
