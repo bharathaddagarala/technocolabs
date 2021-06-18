9799e7b511515e6cb4b28c357e4dc5358465b699
bharathaddagarala <85801717+bharathaddagarala@users.noreply.github.com>
2021-06-18 15:47:28 +0530

    Create index.js

---
+++ projecttask7/index.js
@@ -0,0 +1,17 @@
+const myage = 18;
+
+var earlyYears = 2;
+
+earlyYears = earlyYears*10.5 ;
+
+let laterYears = myage-2;
+
+laterYears = laterYears*4;
+
+let myAgeInDogYears = earlyYears+laterYears ;
+
+const name = 'BHARATH';
+
+let myname = name.toLowerCase();
+
+console.log(`My name is ${myname}. I am ${myage} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
